b1eb2e34ae923ebae1fb1c536337f3acb2d646cd
Matthew Sievers <mattersievers@gmail.com>
2021-10-20 00:53:43 -0500

    replies are functional

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { 
+    addComment, 
+    removeComment,
+    addReply,
+    removeReply  
+} = require('../../controllers/comment-controller');
 
 router
     .route('/:pizzaId')
@@ -8,5 +13,14 @@ router
 router
     .route('/:pizzaId/:commentId')
     .delete(removeComment);
+
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
 
 module.exports = router;
